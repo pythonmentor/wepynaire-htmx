154ad832fa47852d66676c38e0352fb0f4ad6e11
tchappui <tchappui@gmail.com>
2023-10-31 08:33:09 +0100

    Updated settings modules

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,9 +1,29 @@
 from .base import *
 from .base import env
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "SECRET_KEY",
+    default="django-insecure-s_!(-mt*(!omlw-&fn+&%+!i7knohfnh7ke2g9l=e696gh9!by",
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env("DEBUG", default=True)
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    "default": env.db(
+        default="sqlite:///db.sqlite3",
+    )
+}
+
 # Default to dummy email backend. Configure dev/production/local backend
 # as per https://docs.djangoproject.com/en/stable/topics/email/#email-backends
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = env("EMAIL_BACKEND", default='django.core.mail.backends.console.EmailBackend')
 
 INSTALLED_APPS += ["debug_toolbar"]
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,2 +1,15 @@
 from .base import *
 from .base import env
+
+
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = False
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    "default": env.db()
+}
