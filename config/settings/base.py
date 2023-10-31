4931f880e8db251366118950048a9ea6140d7efa
tchappui <tchappui@gmail.com>
2023-10-31 08:46:48 +0100

    Updated settings modules

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,21 +14,6 @@ PROJECT_DIR = BASE_DIR / "config"
 APPS_DIR = BASE_DIR / "example"
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env(
-    "SECRET_KEY",
-    default="django-insecure-s_!(-mt*(!omlw-&fn+&%+!i7knohfnh7ke2g9l=e696gh9!by",
-)
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
-
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
-
-
 # Application definition
 
 INSTALLED_APPS = [
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,2 +1,15 @@
 from .base import *
 from .base import env
+
+
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env.bool("SECRET_KEY", default=False)
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    "default": env.db()
+}
